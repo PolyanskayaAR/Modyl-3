aacf72860eec55f3f37b31a835851de5b10816d2
Polyanskaya Anastaiya Ruslanovna <98982139+PolyanskayaAR@users.noreply.github.com>
2022-04-09 23:04:57 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string MyName = "Nastya";
+            Console.WriteLine(MyName);
+            Console.ReadKey();
+        }
+    }
+}
